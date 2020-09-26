65cd4d4312d0df0a422cc4798682960f64d9686e
Macarealeportelli <macarenarealeportelli@gmail.com>
2020-09-25 21:44:35 -0300

    hacemos funcionar filtro del input y checkbox por separado dentro de pasaFiltro

--- index.js
+++ index.js
@@ -51,14 +51,118 @@ const filtroColor = document.querySelectorAll("input[type='checkbox']");
 //   };
 // }
 
+//chequea si hay algun checbox chequeado
 const hayAlgunCheckBoxChequeado = () => {
-  for (checkbox of filtroColor) {
-    if (checkbox.checked) {
-      return true
+    for (checkbox of filtroColor) {
+        if (checkbox.checked) {
+            return true
+        }
     }
-  }
-  return false
+    return false
 }
+
+// chequeamos si los checkbox coinciden con alguna tarjeta
+const comparoColorEnLaTarjeta = (card) => {
+   
+    if (card.dataset.color === checkbox.value){
+        return true
+    }
+    else {
+        return false
+    }
+}
+
+
+
+
+
+
+
+//chequea si hay algun radio chequeado
+const hayAlgunRadioChequeado = () => {
+    for (radio of filtroSexo) {
+        if (radio.checked) {
+            return true
+        }
+    }
+    return false
+}
+
+//chequea si hay algo escrito en el input
+const hayAlgoEscritoEnElInput = () => {
+    if (filtroBusqueda.value) {
+        return true
+    }
+    else {
+        return false
+    }
+}
+
+//chequea si las tarjetas coinciden con el input
+const compararInputConTarjeta = (card) => {
+    if (card.dataset.nombre.includes(filtroBusqueda.value)) {
+        return true
+    }
+    else {
+        return false
+    }
+}
+
+
+
+const pasaFiltros = (card) => {
+    //me fijo si hay algo escrito en el input
+    // si hay algo escrito reviso que coincida con el nombre de la tarjeta
+    // si coincide retorna true
+    // si no cincide retorna false
+
+
+    // if (hayAlgoEscritoEnElInput()) {
+    //     if (compararInputConTarjeta()) {
+    //         return true
+    //     }
+    //     else {
+    //         return false
+    //     }
+    // }
+    // else {
+    //     return true
+    // }
+
+// me fijo si hay algo chequeado en los checkbox
+// si lo hay, me fijo que checkbox está chequeado
+// si el radio chequeado coinciden con el color de alguna tarjeta
+// retorno true, sino retorno false
+
+if (hayAlgunCheckBoxChequeado()){
+    if (comparoColorEnLaTarjeta(card)){
+        return true
+    }
+    else {
+        return false
+    }
+}
+else {
+    return true
+}
+
+
+
+
+
+
+
+// me fijo si hay algo chequeado en los radio
+// si lo hay, me fijo que radio está chequeado
+// si el radio chequeado coinciden con el sexo de alguna tarjeta
+// retorno true, sino retorno false
+
+
+}
+
+
+
+
 
 
 // pasaFiltros -> param card -> return true o false 
@@ -71,38 +175,41 @@ const hayAlgunCheckBoxChequeado = () => {
 
 
 const ocultarTarjeta = (card) => {
-  return card.classList.add("hidden")
+    return card.classList.add("hidden")
 }
 
 const mostrarTarjeta = (card) => {
-   return card.classList.remove("hidden")
+    return card.classList.remove("hidden")
 }
 
 const filtrarTarjetas = () => {
-  for (let card of cards) {
-    if (pasaFiltros(card)) {
-      mostrarTarjeta(card)
+    for (let card of cards) {
+        if (pasaFiltros(card)) {
+            mostrarTarjeta(card)
+        }
+        else {
+            ocultarTarjeta(card)
+        }
     }
-    else {
-      ocultarTarjeta(card)
-    }
-  }
 }
 
+
+
+
 filtroBusqueda.oninput = () => {
-  filtrarTarjetas()
+    filtrarTarjetas()
 }
 
 for (let checkbox of filtroColor) {
-  checkbox.oninput = () => {
-    filtrarTarjetas()
-  }
+    checkbox.oninput = () => {
+        filtrarTarjetas()
+    }
 }
 
 for (let radio of filtroSexo) {
-  radio.oninput = () => {
-    filtrarTarjetas()
-  }
+    radio.oninput = () => {
+        filtrarTarjetas()
+    }
 }
 
 
